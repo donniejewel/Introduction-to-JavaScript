f0ea924bada874590f8e4eed0b71539822c1844c
Donnie Jewel <donnie-hassett@lambdastudents.com>
2020-07-27 00:49:15 -0500

    Still learning the JavaScript concept

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
-//Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
+//Task a: declare a variable called votingAge, 
+// console log true if age > 18 (no function required)
 
-
+const votingAge= 18;
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
-
 
 
 
